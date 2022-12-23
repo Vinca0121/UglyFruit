c1e243394fa9a891117be9c40a977e0aa12461db
Vinca0121 <98202797+Vinca0121@users.noreply.github.com>
2022-12-23 12:23:11 +0900

    merge
    merge in to profile

--- Assets/Scripts/spawnFruit.cs
+++ Assets/Scripts/spawnFruit.cs
@@ -16,6 +16,7 @@ public class spawnFruit : MonoBehaviour
 
     float currTime;
     public int result;
+    public float time;
     void Start()
     {
         result = 0;
@@ -26,50 +27,50 @@ public class spawnFruit : MonoBehaviour
 
 
         currTime += Time.deltaTime;
-        if (currTime > 3)
+        if (currTime > time)
         {
             result = result + 1;
             if (result == 1)
             {
                 GameObject b = Instantiate(FruitFactory1);
-                b.transform.position = FruitPosition.transform.position + Vector3.right + Vector3.up;
-                Destroy(b, 3);
+                b.transform.position = FruitPosition.transform.position;
+                Destroy(b, time);
             }
             else if(result == 2)
             {
                 GameObject b = Instantiate(FruitFactory2);
                 b.transform.position = FruitPosition.transform.position;
-                Destroy(b, 3);
+                Destroy(b, time);
             }
             else if (result == 3)
             {
                 GameObject b = Instantiate(FruitFactory3);
                 b.transform.position = FruitPosition.transform.position;
-                Destroy(b, 3);
+                Destroy(b, time);
             }
             else if (result == 4)
             {
                 GameObject b = Instantiate(FruitFactory4);
                 b.transform.position = FruitPosition.transform.position;
-                Destroy(b, 3);
+                Destroy(b, time);
             }
             else if (result == 5)
             {
                 GameObject b = Instantiate(FruitFactory5);
                 b.transform.position = FruitPosition.transform.position;
-                Destroy(b, 3);
+                Destroy(b, time);
             }
             else if (result == 6)
             {
                 GameObject b = Instantiate(FruitFactory6);
                 b.transform.position = FruitPosition.transform.position;
-                Destroy(b, 3);
+                Destroy(b, time);
             }
             else if (result == 7)
             {
                 GameObject b = Instantiate(FruitFactory7);
                 b.transform.position = FruitPosition.transform.position;
-                Destroy(b, 3);
+                Destroy(b, time);
                 result = 0;
             }
 
